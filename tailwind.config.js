cb3c3efbff240b772f1f1249d68093dfefe33f7c
B Manoj Kiran <kiran797979@gmail.com>
2025-06-26 11:02:06 +0530

    Fresh project setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./resources/js/**/*.{js,ts}",
+    "./resources/data/**/*.{js,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}; 
